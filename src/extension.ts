2d3d6dc7786db12277670075956a3dd0e9fa9e25
Chris Wendt <chrismwendt@gmail.com>
2018-09-24 21:47:15 -0700

    cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,46 @@
-import * as sourcegraph from "sourcegraph";
+import * as sourcegraph from 'sourcegraph'
+import * as rpc from 'vscode-ws-jsonrpc'
 
-import * as rpc from "vscode-ws-jsonrpc";
+const PORT = 1234
 
 export function activate(): void {
-  const webSocket = new WebSocket("ws://localhost:1234");
-  rpc.listen({
-    webSocket,
-    onConnection: (connection: rpc.MessageConnection) => {
-      connection.listen();
+    const webSocket = new WebSocket('ws://localhost:' + PORT)
+    rpc.listen({
+        webSocket,
+        onConnection: (connection: rpc.MessageConnection) => {
+            connection.listen()
 
-      sourcegraph.languages.registerHoverProvider(["*"], {
-        provideHover: async (doc, pos) => {
-          return connection
-            .sendRequest<sourcegraph.Hover>("hov", doc, pos)
-            .then(v => {
-              console.log("recv response hov", v);
-              return (
-                v && {
-                  contents: {
-                    value:
-                      "```python\n" + (v.contents as any).join("\n") + "\n```",
-                    kind: sourcegraph.MarkupKind.Markdown
-                  }
-                }
-              );
-            });
-        }
-      });
+            sourcegraph.languages.registerHoverProvider(['*'], {
+                provideHover: async (doc, pos) => {
+                    return connection.sendRequest<sourcegraph.Hover>('hover', doc, pos).then(hover => {
+                        return (
+                            hover && {
+                                contents: {
+                                    value: '```python\n' + (hover.contents as any).join('\n') + '\n```',
+                                    kind: sourcegraph.MarkupKind.Markdown,
+                                },
+                            }
+                        )
+                    })
+                },
+            })
 
-      sourcegraph.languages.registerDefinitionProvider(["*"], {
-        provideDefinition: async (doc, pos) => {
-          return connection.sendRequest<any>("def", doc, pos).then(v => {
-            console.log("recv response def", v);
-            return (
-              v &&
-              new sourcegraph.Location(
-                new sourcegraph.URI(doc.uri),
-                new sourcegraph.Range(
-                  new sourcegraph.Position(v.start.line, v.start.character),
-                  new sourcegraph.Position(v.end.line, v.end.character)
-                )
-              )
-            );
-          });
-        }
-      });
-    }
-  });
+            sourcegraph.languages.registerDefinitionProvider(['*'], {
+                provideDefinition: async (doc, pos) => {
+                    return connection.sendRequest<any>('definition', doc, pos).then(definition => {
+                        return (
+                            definition &&
+                            new sourcegraph.Location(
+                                new sourcegraph.URI(doc.uri),
+                                new sourcegraph.Range(
+                                    new sourcegraph.Position(definition.start.line, definition.start.character),
+                                    new sourcegraph.Position(definition.end.line, definition.end.character)
+                                )
+                            )
+                        )
+                    })
+                },
+            })
+        },
+    })
 }
